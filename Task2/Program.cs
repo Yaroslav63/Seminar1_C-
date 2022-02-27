861ace9663613162f62d5e551fc2d98ff690c20a
Yaroslav63 <era6300@mail.ru>
2022-02-27 19:03:49 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine("Максимальное число" );
+    Console.WriteLine(number1);
+}
+
+if(number1 < number2)
+{
+    Console.WriteLine("Максимальное число" );
+    Console.WriteLine(number2);
+}
